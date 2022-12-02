c88c15ac62efbf06fc7e94e27d89c8f01a1f79d9
Daniel Gomez <dgomez.informatica@gmail.com>
2022-12-02 01:02:21 -0300

    add: Clase principal donde se llama a la funcion adicion del directorio includes, mostrando el resultado de la suma

---
+++ multiples_argumentos_csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using multiples_argumentos_csharp.includes;
+using System;
+
+namespace multiples_argumentos_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions func = new Functions();
+
+            Console.WriteLine($"El resultado de la suma (2+2+8) es: {func.adicion(2,2,8)}");
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
