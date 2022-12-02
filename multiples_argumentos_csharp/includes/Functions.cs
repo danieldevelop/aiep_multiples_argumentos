006e24726bb88c4b8ca95cf6c2b33e9dd066b883
Daniel Gomez <dgomez.informatica@gmail.com>
2022-12-02 00:58:18 -0300

    add: Se crea funcion que permite recibir n cantidad de parametros, retornando la suma de ellos

---
+++ multiples_argumentos_csharp/includes/Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace multiples_argumentos_csharp.includes
+{
+    internal class Functions
+    {
+        private int resultadoSuma;
+
+        /**
+         * Esta funcion o metodo, recibe n cantidad de valores numericos, es decir
+         * que puede recibir 2 o más valores para para operar
+         */
+        public int adicion(params int[] numero)
+        {
+            foreach (int num in numero)
+            {
+                resultadoSuma += num;
+            }
+
+            return resultadoSuma;
+        }
+
+    }
+}
